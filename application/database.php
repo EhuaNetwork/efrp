37aaeea4e5c161e665549469242fcbe5d8265700
Ehua <ehua999@163.com>
2022-01-11 10:51:09 +0800

    更新

--- application/database.php
+++ application/database.php
@@ -2,9 +2,9 @@
 return 
 array (
   'hostname' => '127.0.0.1',
-  'database' => '',
-  'username' => '',
-  'password' => '',
+  'database' => 'frpc_cn',
+  'username' => 'frpc_cn',
+  'password' => 'frpc_cn',
   'hostport' => '3306',
   'prefix' => 'ecms_',
 )
